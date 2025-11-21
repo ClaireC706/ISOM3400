76e563045e17b6cf78d53c6f9da039553ef1b1cb
ClaireC706 <zchenie@connect.ust.hk>
2025-11-21 09:52:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import time
+
 st.title("Business Dashboard with Streamlit Layouts")
 
 msg = ("Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard.")
