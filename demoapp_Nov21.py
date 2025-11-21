c7a98cd96d2fcb9310e7d859719db67f44d66812
ClaireC706 <zchenie@connect.ust.hk>
2025-11-21 10:12:56 +0800

    Update demoapp_Nov21.py

--- demoapp_Nov21.py
+++ demoapp_Nov21.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 from numpy.random import default_rng as rng
 
+df = pd.DataFrame(rng(0).standard_normal((20,3)), columns = ["a","b","c"])
+st.area_chart(df)
+
 option = st.selectbox(
     "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
+    ("Email", 
+     "Home phone", 
+     "Mobile phone"),
 )
 
 st.write("You selected:", option)
